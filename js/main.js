360d62d4903ab8ee3caedd74bc3131d8e8bb6b7c
andremakoski12 <andremakoski@users.noreply.github.com>
2020-06-26 19:39:43 -0300

    feito

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            //alert(response.logradouro);
+        }
+    })
+}
